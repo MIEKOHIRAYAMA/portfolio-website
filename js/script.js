3a927531f3e2184f877e00f011a73caf929068b4
MIEKOHIRAYAMA <55162204+MIEKOHIRAYAMA@users.noreply.github.com>
2019-10-01 21:01:51 +0100

    Updated hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,20 +1,12 @@
 console.log("Hello World, I'm Mieko");
 
-function menuToggle() {
-    var x = document.getElementsByClassName('hidden');
-    for(var i = 0; i < x.length; i++) {
-           if (x[i].style.display !== 'table') {
-               x[i].style.display = 'table'
-           } else {
-               x[i].style.display = 'none'
-           }
-}}
 
-// function menuToggle() {
-//     var x = document.getElementById('myNavtoggle');
-//     if (x.className === 'navtoggle') {
-//       x.className = ' responsive';
-//     } else {
-//       x.className = 'navtoggle';
-//     }
-//   }
+
+(() => {
+    $(document).ready(() => {
+       
+      $('.hamburger-menu-button').click(() => {
+        $('ul').toggleClass('show-menu-item');
+      })
+    });
+  })();
